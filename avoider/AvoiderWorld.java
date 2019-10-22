60c391f98530c044f6b9fdbf869cf0582bc01711
Eloy <eloyhz@gmail.com>
2019-10-22 10:58:06 -0500

    Enemy added
    The enemy falls and disapears at the bottom of the world

--- avoider/AvoiderWorld.java
+++ avoider/AvoiderWorld.java
@@ -16,6 +16,24 @@ public class AvoiderWorld extends World
     public AvoiderWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(600, 400, 1, false); 
+    }
+    
+    public void act()
+    {
+        if (Greenfoot.getRandomNumber(100) < 2) {
+            Enemy e = new Enemy();
+            int x = Greenfoot.getRandomNumber(getWidth()-20) + 10;
+            int y = -30;
+            super.addObject(e, x, y);
+        }
     }
 }
+
+
+
+
+
+
+
+
